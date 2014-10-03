bdeaada8b5fd911d07021121662db711194b5d1c
Michael Whitman <michael@whitman.com.au>
2014-10-03 21:11:12 +1000

    switching animation test on hero rimon

--- src/assets/chars/hero.lua
+++ src/assets/chars/hero.lua
@@ -2,16 +2,31 @@ local hero = {
 
 }
 
-local walk = require 'assets.animations.hero_walk';
+local walk = require 'assets.animations.hero_walk'
+local retreat = require 'assets.animations.hero_retreat'
+local active = walk
 
 function update(dt)
-	walk.animation:update(dt)
+	if active == walk then
+		if walk.done == true then		
+			active = retreat
+		else
+		    walk.animation:update(dt)
+		end		
+	end
+	if active == retreat then
+		if retreat.done == true then
+			active = walk
+		else
+			retreat.animation:update(dt)
+		end
+	end
 end
 
 function draw()
 	--draw test anim
 	love.graphics.setColor(255, 255, 255, 255)
-	walk.animation:draw(walk.hero_walk_spritemap, 40, 40)
+	active.animation:draw(active.spritemap, 40, 40)
 end
 
 hero.update = update
