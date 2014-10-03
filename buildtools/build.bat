6b02762d1022587759b6ee76f77ae2e36396b970
Cameron B <x0p30d3@gmail.com>
2014-10-03 19:32:08 +1000

    Added build scripts for win_x64

---
+++ buildtools/build.bat
@@ -0,0 +1,45 @@
+@echo off
+
+:: [make] clean
+echo Cleaning files
+@pushd ..
+del /s /q /f release > NUL
+rmdir /s /q release
+@popd
+
+:: make
+echo Making release dir
+@pushd ..
+mkdir release
+mkdir release\temp
+
+echo Copying files
+xcopy /i buildtools\lib release\temp > NUL
+xcopy /i /e src release\temp > NUL
+copy buildtools\exe\love.exe release\love.exe > NUL
+@popd
+
+:: -- zipping
+echo Zipping files
+@pushd exe
+7za a -tzip ..\..\release\main.zip ..\..\release\temp\* -r > NUL
+@popd
+
+:: build
+@echo Building exe
+@pushd ..\release
+copy /b love.exe+main.zip game.exe > NUL
+@popd
+
+:: clean up
+echo Cleaning up
+@pushd ..\release
+del /s /f /q temp > NUL
+rmdir /s /q temp > NUL
+del /f /q main.zip > NUL
+del /f /q love.exe > NUL
+@popd
+
+echo Done
+
+pause
---
+++ buildtools/clean.bat
@@ -0,0 +1,8 @@
+@echo off
+
+:: [make] clean
+echo Cleaning files
+@pushd ..
+del /s /q /f release > NUL
+rmdir /s /q release
+@popd
