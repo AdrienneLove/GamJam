09af2d06018beb1043bbe67c950d492348f4dc31
Adrian Love <aijokaizeru@gmail.com>
2014-10-03 23:48:25 +1000

    added in a foreground + background implementation for background. will re-aproach solution with more sleep. testicles.

--- src/states/level.lua
+++ src/states/level.lua
@@ -14,7 +14,13 @@ local status = "intro" -- other states are play, dead and outro
 
 
 -- hot swap between panel 1, 2 and 3
-local panels = {
+local background_panels = {
+	{ x = 0, image = nil, current = false, furthest = false},
+	{ x = 0, image = nil, current = false, furthest = false},
+	{ x = 0, image = nil, current = false, furthest = true}
+}
+
+local foreground_panels = {
 	{ x = 0, image = nil, current = false, furthest = false},
 	{ x = 0, image = nil, current = false, furthest = false},
 	{ x = 0, image = nil, current = false, furthest = true}
@@ -27,32 +33,51 @@ function level:enter(state)
 	--panel iterates at half screen
 	current_panel = 1
 
-	--load in background panel data
+	--load in panel data for foreground and background
 	background_imagedata_start = love.image.newImageData('assets/images/start.gif')
-	background_imagedata_1 = love.image.newImageData('assets/images/224x126.gif')
+	background_imagedata_1 = love.image.newImageData('assets/images/wall1sketch.png')
+	foreground_imagedata_1 = love.image.newImageData('assets/images/floor1sketch.png')
 	background_imagedata_end = love.image.newImageData('assets/images/end.gif')
 
-	--create as many image items as needed
-	background_panels = { 
+	--create as many image items as needed for background then foreground
+	background_panel_images = { 
 		love.graphics.newImage(background_imagedata_1),
 		love.graphics.newImage(background_imagedata_1),
 		love.graphics.newImage(background_imagedata_1),
 		love.graphics.newImage(background_imagedata_1)
 	}
 
-	--build initial panels
-	for key,value in pairs(panels) do 
-		
+	foreground_panel_images = {
+		love.graphics.newImage(foreground_imagedata_1),
+		love.graphics.newImage(foreground_imagedata_1),
+		love.graphics.newImage(foreground_imagedata_1),
+		love.graphics.newImage(foreground_imagedata_1)
+	}
+
+	--build initial background panels
+	for key,value in pairs(background_panels) do 
 		value.x = background_imagedata_1:getWidth() * (key-1)
 
    		if key == 1 then
    			value.image = love.graphics.newImage(background_imagedata_start)
    			value.current = true
    		else 
-   			value.image = background_panels[math.random(table.getn(background_panels))]
+   			value.image = background_panel_images[math.random(table.getn(background_panel_images))]
    			value.current = false
    		end
+	end
 
+	--build initial foreground panels
+	for key,value in pairs(foreground_panels) do 
+		value.x = foreground_imagedata_1:getWidth() * (key-1)
+		value.image = foreground_panel_images[math.random(table.getn(foreground_panel_images))]
+   		
+   		if key == 1 then
+   			value.current = true
+   		else 
+   			
+   			value.current = false
+   		end
 	end
 
 	-- set timer to go from intro to play
@@ -70,29 +95,72 @@ function level:update(dt)
 	Timer.update(dt)
 
 	if status == "play" then
-		-- check for panel out of range and slap another on the end!
-		for key, value in pairs(panels) do 
+
+		-- move background panels
+		for key, value in pairs(background_panels) do 
 			-- move panels
 			value.x = value.x - level_speed * dt
 
 			if value.x <  background_imagedata_1:getWidth()*-1 then
 
 				-- set item values and assign a new random image
-				value.image = background_panels[math.random(table.getn(background_panels))]
+				value.image = background_panel_images[math.random(table.getn(background_panel_images))]
 				value.x = background_imagedata_1:getWidth()*2
 				value.current = false
 
 				-- set current panel
-				if key+1 > table.getn(panels) then
-					panels[1].current = true
+				if key+1 > table.getn(background_panels) then
+					background_panels[1].current = true
 				else 
-					panels[key+1].current = false
+					background_panels[key+1].current = false
 				end
 
 				--search furthest and replace it, set this x to new furthest location
-				for furthest_ket, furthest_value in pairs(panels) do 
+				for furthest_ket, furthest_value in pairs(background_panels) do 
 					if furthest_value.furthest then 
-						value.x = furthest_value.x + furthest_value.image:getWidth()
+						-- don't touch this. wizardy ahead. (can't touch this)
+						if key == 1 then
+							value.x = furthest_value.x + furthest_value.image:getWidth()	 - level_speed * dt
+						else
+							value.x = furthest_value.x + furthest_value.image:getWidth()
+						end
+						furthest_value.furthest = false
+						value.furthest = true
+						--done searching so break
+						break
+					end
+				end
+			end
+		end
+
+		-- move foreground panels
+		for key, value in pairs(foreground_panels) do 
+			-- move panels
+			value.x = value.x - level_speed * dt
+
+			if value.x <  foreground_imagedata_1:getWidth()*-1 then
+
+				-- set item values and assign a new random image
+				value.image = foreground_panel_images[math.random(table.getn(foreground_panel_images))]
+				value.x = background_imagedata_1:getWidth()*2
+				value.current = false
+
+				-- set current panel
+				if key+1 > table.getn(foreground_panels) then
+					foreground_panels[1].current = true
+				else 
+					foreground_panels[key+1].current = false
+				end
+
+				--search furthest and replace it, set this x to new furthest location
+				for furthest_ket, furthest_value in pairs(foreground_panels) do 
+					if furthest_value.furthest then 
+						-- don't touch this. wizardy ahead. (can't touch this)
+						if key == 1 then
+							value.x = furthest_value.x + furthest_value.image:getWidth()	 - level_speed * dt
+						else
+							value.x = furthest_value.x + furthest_value.image:getWidth()
+						end
 						furthest_value.furthest = false
 						value.furthest = true
 						--done searching so break
@@ -106,11 +174,16 @@ end
 
 function level:draw()
 	love.graphics.push()
-	
+
 	love.graphics.scale( SCALE )
 
-	--draw panels 1, 2 and 3
-	for key, value in pairs(panels) do 
+	--draw background panels 1, 2 and 3
+	for key, value in pairs(background_panels) do 
+		love.graphics.draw(value.image, value.x, 0)
+	end
+
+	--draw foreground panels 1, 2 and 3
+	for key, value in pairs(foreground_panels) do 
 		love.graphics.draw(value.image, value.x, 0)
 	end
 	
