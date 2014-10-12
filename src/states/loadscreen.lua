6e5699304ba4598d669dc6bf76d01266c259ba74
Michael Whitman <michael@whitman.com.au>
2014-10-12 12:54:31 +1000

    inserted arrow image

--- src/states/loadscreen.lua
+++ src/states/loadscreen.lua
@@ -2,19 +2,9 @@ local tween = require "lib.tween"
 
 local loadscreen = {}
 
-loading_screens = {
-		love.graphics.newImage('assets/images/level2.png'),
-		love.graphics.newImage('assets/images/level3.png'),
-		love.graphics.newImage('assets/images/level4.png'),
-		love.graphics.newImage('assets/images/level5.png'),
-		love.graphics.newImage('assets/images/level5.png'),
-		love.graphics.newImage('assets/images/level5.png'),
-		love.graphics.newImage('assets/images/level5.png'),
-		love.graphics.newImage('assets/images/level5.png'),
-		love.graphics.newImage('assets/images/level5.png')
-	}
+loading_screens = love.graphics.newImage('assets/images/loading_screen_dummy.png')
 
-local arrow_image = love.graphics.newImage("assets/images/colourGreen.png")
+local arrow_image = love.graphics.newImage("assets/images/levelarrow.png")
 
 local initial = {x = 533, y = 137}
 local offset = {x = -16, y = 40}
@@ -25,8 +15,7 @@ local bounceoffset = {x = 0}
 local bouncey = tween.new(1, bounceoffset, {x = 50}, "outCirc")
 
 function loadscreen:draw(cur_level)
-	loadback = loading_screens[cur_level]
-	love.graphics.draw(loadback, 0, 0, 0, love.graphics.getWidth() / loadback:getWidth(), love.graphics.getHeight() / loadback:getHeight())
+	love.graphics.draw(loading_screens, 0, 0, 0, love.graphics.getWidth() / loading_screens:getWidth(), love.graphics.getHeight() / loading_screens:getHeight())
 
 	local complete = bouncey:update(0.01)
 	if complete == true then
@@ -34,7 +23,7 @@ function loadscreen:draw(cur_level)
 	end
 	arrow_x = initial.x + offset.x * cur_level + bounceoffset.x
 	arrow_y = initial.y + offset.y * cur_level
-	love.graphics.draw(arrow_image, arrow_x, arrow_y, 0, love.graphics.getWidth() / loadback:getWidth(), love.graphics.getHeight() / loadback:getHeight())
+	love.graphics.draw(arrow_image, arrow_x, arrow_y, 0, love.graphics.getWidth() / loading_screens:getWidth(), love.graphics.getHeight() / loading_screens:getHeight())
 end
 
 return loadscreen
